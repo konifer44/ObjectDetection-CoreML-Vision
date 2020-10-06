f6b40a935dcd1af457ae39aced5c883c21344aef
konifer44 <konifer44@gmail.com>
2020-10-06 13:18:04 +0200

    Initial Commit

---
+++ MLObjectDetection/MLObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLObjectDetectionApp.swift
+//  MLObjectDetection
+//
+//  Created by Jan Konieczny on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MLObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
